f9774c1597470479e50953103bb936fba1a80128
aslam48 <youngfizzy001@gmail.com>
2023-07-17 14:38:38 +0100

    working

---
+++ src/navigation/TabNavigation.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const TabNavigation = () => {
+  return (
+    <View>
+      <Text>TabNavigation</Text>
+    </View>
+  )
+}
+
+export default TabNavigation
+
